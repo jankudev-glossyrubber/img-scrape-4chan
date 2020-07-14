100283edefe05ca71216a253251423c0c5f5b76e
Tomas Janku <janku.tomas@gmail.com>
2020-07-15 01:36:17 +0200

    Updated scraping list

--- run.sh
+++ run.sh
@@ -33,7 +33,7 @@ printBlock "Latex"
 ./scrape.sh s latex "${DEST}/img/latex"
 
 printBlock "B.D.S.M."
-./scrape.sh s 'bondage|bdsm' "${DEST}/img/bondage"
+./scrape.sh s 'bondage|bdsm|dominant|mistress' "${DEST}/img/bondage"
 
 printBlock "Swim suits"
 ./scrape.sh s 'swimsuit|bikini' "${DEST}/img/swimsuit"
@@ -48,3 +48,13 @@ printBlock "Blonde"
 printBlock "You become the girl below"
 ./scrape.sh s 'you become the girl' "${DEST}/img/ybtgb"
 ./scrape.sh hr 'you become the girl' "${DEST}/img/ybtgb"
+
+printBlock "Women in fur"
+./scrape.sh s 'fur thread' "${DEST}/img/fur"
+
+printBlock "Wallpapers"
+./scrape.sh hr 'current wallpaper' "${DEST}/img/wallpapers"
+./scrape.sh wg 'nature|mountain|forrest' "${DEST}/img/wallpapers"
+
+printBlock "Sci-fi, cyberpunk and steampunk"
+./scrape.sh wg 'sci-?fi||science|fractal|math|steampunk|cyberpunk' "${DEST}/img/scifi"
